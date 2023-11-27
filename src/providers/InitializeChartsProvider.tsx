df1fa1308d175a23e243240c5b9bd95d408e4286
lucas cortez <lucasc.sanches65@gmail.com>
2023-11-27 12:24:41 -0300

    fix: init chart provider

--- src/providers/InitializeChartsProvider.tsx
+++ src/providers/InitializeChartsProvider.tsx
@@ -6,7 +6,6 @@ import { useOrderStore } from "@/stores/useOrderStore";
 import { useStrategyStore } from "@/stores/useStrategyStore";
 import { useSession } from "next-auth/react";
 import { ReactNode, useEffect } from "react";
-import { useStoreInContext } from "./ZustandProvider";
 
 export const InitializeChartsProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const { initializeTrades } = useTrade();
@@ -14,7 +13,6 @@ export const InitializeChartsProvider: React.FC<{ children: ReactNode }> = ({ ch
   const loaded = useStrategyStore((state) => state.loaded);
   const setOrders = useOrderStore((state) => state.setOrders);
   const { data } = useSession();
-  const add = useStoreInContext((state) => state.add);
 
   useEffect(() => {
     if (data?.accessToken)
@@ -33,11 +31,6 @@ export const InitializeChartsProvider: React.FC<{ children: ReactNode }> = ({ ch
     if (loaded) initializeTrades();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [loaded]);
-
-  useEffect(() => {
-    // @ts-ignore
-    add(4);
-  }, [add]);
 
   return children;
 };
